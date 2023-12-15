45f70ee95db10cd3c221db4bf1d58c63fe53c683
X1 <vo.nguyen@aiesec.net>
2023-12-15 17:20:53 +0700

    change button color to mau xanh

--- app/src/main/java/com/example/myhospitalapp/MainActivity.java
+++ app/src/main/java/com/example/myhospitalapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myhospitalapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 //                MainActivity.this.finish();
             }
         });
+        binding.doctorButton.setBackgroundColor(Color.CYAN);
         binding.doctorButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
